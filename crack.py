01dcbcb7d7a14dde3a68b9bff57551a049ed1123
McSam-py <ardisam9876@gmail.com>
2023-03-03 01:07:03 +0000

    doing some bug fixes

--- crack.py
+++ crack.py
@@ -15,43 +15,51 @@ def get_args():
 
 def crack_word_chunks(chunk: list, hash: str, hashformat: str, result_queue):
     for word in chunk:
-        if hash == hashsum( hashformat, word.strip()):
-            result_queue.put((True, word))
-            return
+        if len(word.strip()) <= 30: 
+            print(f"\r[+] Trying --> {word.strip()}"+" "*(80-len(word)), end="")
+
+        if hash == hashsum(hashformat, word):
+            return result_queue.put((True, word))
 
         result_queue.put((False, None))
+    
 
 def crack(hash: str, hashformat: str, wordlist: str):
     
-    result_queue = multiprocessing.Queue()
+    try:
+        result_queue = multiprocessing.Queue()
 
-    with open(wordlist, 'r', encoding="iso-8859-1") as dictfile:
-        words = dictfile.readlines()
+        with open(wordlist, 'r', encoding="ISO-8859-1") as dictfile:
+            words = dictfile.readlines()
 
-    num_of_cores = multiprocessing.cpu_count()
+        num_of_cores = multiprocessing.cpu_count()
 
-    chunk_size = len(words) // num_of_cores
+        chunk_size = len(words) // num_of_cores
 
-    chunks = [words[i:i+chunk_size] for i in range(0, len(words), chunk_size)]
+        chunks = [words[i:i+chunk_size] for i in range(0, len(words), chunk_size)]
 
-    processes = []
+        processes = []
 
-    for chunk in chunks:
-        process = multiprocessing.Process(target=crack_word_chunks, \
-                                          args=(chunk, hash, hashformat, result_queue))
-        processes.append(process)
-        process.start()
+        for chunk in chunks:
+            process = multiprocessing.Process(target=crack_word_chunks, \
+                                            args=(chunk, hash, hashformat, result_queue))
+            processes.append(process)
+            process.start()
 
-    for process in processes:
-        process.join()
+        for process in processes:
+            process.join()
 
-    while not result_queue.empty():
-        result = result_queue.get()
-        if result[0]:
-            print("[+] Hash has been cracked.")
-            print(f"[HASH] {hashformat} --> {hash} --> {result[1]}", end="")
-            return
-    
+        while not result_queue.empty():
+            result = result_queue.get()
+            if result[0]:
+                print("\n[+] Hash has been cracked.")
+                print(f"[HASH] {hashformat} --> {hash} --> {result[1]}", end="")
+                break
+            
+    except KeyboardInterrupt:
+        print("[-] CTRL-C detected, exiting...")
+        sys.exit()
+        
 
 def hashsum(hashformat:str, password: str):
 
@@ -69,5 +77,8 @@ def hashsum(hashformat:str, password: str):
 if __name__ == "__main__":
     arguments = get_args()
     hash, hashformat, wordlist = arguments.hash, arguments.format, arguments.wordlist
-    crack(hash, hashformat, wordlist)
+    try:
+        crack(hash, hashformat, wordlist)
+    except KeyboardInterrupt:
+        sys.exit("[-] CTRL-C detected, exiting...")
 
